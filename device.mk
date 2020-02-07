191c77a8f52fed93657329b1b421d7a84944eaaa
travisholt92 <travisholt92@gmail.com>
2020-02-06 22:32:05 -0500

    Decommonize camera and FoD
    Co-authored-by: name <inderpreetbhangu2@gmail.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,7 @@
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
+$(call inherit-product, device/oneplus/settings/common.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/guacamoleb/guacamoleb-vendor.mk)
@@ -26,6 +27,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_11811.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_11811.xml \
     $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
+    
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libxml2 \
+    vendor.oneplus.camera.CameraHIDL@1.0.vendor \
+    Snap
 
 # Device init scripts
 PRODUCT_PACKAGES += \
